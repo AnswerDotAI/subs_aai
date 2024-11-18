12b65ef7db6a3626b75dcc8a08fb5794264d9f0e
Jeremy Howard <github@jhoward.fastmail.fm>
2024-11-18 10:27:29 +1000

    update submodules

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git pull -X theirs >/dev/null 2>&1
+git pull -X ours >/dev/null 2>&1
 git submodule foreach --quiet 'echo $path' | xargs -P0 -I{} sh -c 'cd {} && git pull -q origin main'
 git commit -am "update submodules" && git push -f
