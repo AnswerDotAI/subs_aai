8351ce89e4301fb84bead7ba53c0f028536c4b48
Jeremy Howard <info@fast.ai>
2024-11-24 01:52:54 +0000

    update submodules

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-git pull -X ours >/dev/null 2>&1
+#git pull -X ours >/dev/null 2>&1
+git fetch origin main
+git merge -X ours origin/main >/dev/null 2>&1
 git submodule foreach --quiet 'echo $path' | xargs -P0 -I{} sh -c 'cd {} && git pull -q origin main'
 git commit -am "update submodules" && git push -f
